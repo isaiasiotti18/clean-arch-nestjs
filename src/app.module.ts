ee8a890f1d7c999fccc817c3eed8382b0f5bdccc
Isaias Batista dos Santos <isaiasiottiprofissional@gmail.com>
2022-10-20 10:37:57 -0300

    nest g res routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { RoutesModule } from './routes/routes.module';
 
 @Module({
-  imports: [],
+  imports: [RoutesModule],
   controllers: [],
   providers: [],
 })
